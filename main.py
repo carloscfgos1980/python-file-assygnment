552d9213826ce04a1fa8cbe7913244d625f63b49
Carlos Infante <kuva5008@gmail.com>
2022-12-22 06:24:19 +0100

    error part 3

---
+++ main.py
@@ -0,0 +1,35 @@
+
+__winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
+__human_name__ = "files"
+
+
+# import os
+# import zipfile
+
+import sys
+
+sys.path.append(r'/Users/carlosinfante/Documents/Winc/files')
+print(sys.path)
+
+'''
+def clean_cache():
+    return os.mkdir('/Users/carlosinfante/Documents/Winc/files/cache')
+
+
+print(clean_cache())
+'''
+
+'''
+def cache_zip(zip_path, dir_path):
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(dir_path)
+
+
+print(cache_zip('/Users/carlosinfante/Documents/Winc/files/data.zip',
+                '/Users/carlosinfante/Documents/Winc/files/cache'))
+'''
+
+
+# def cached_files():
+
+# def find_password()
