6cfd8ab65a23c69298d3cc1b2eb6351f919e4482
Carlos Infante <kuva5008@gmail.com>
2022-12-24 04:56:01 +0100

    complete

--- main.py
+++ main.py
@@ -3,23 +3,18 @@ __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
 
-# import os
-# import zipfile
+import os
+import zipfile
+import os
 
-import sys
 
-sys.path.append(r'/Users/carlosinfante/Documents/Winc/files')
-print(sys.path)
-
-'''
 def clean_cache():
     return os.mkdir('/Users/carlosinfante/Documents/Winc/files/cache')
 
 
 print(clean_cache())
-'''
 
-'''
+
 def cache_zip(zip_path, dir_path):
     with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(dir_path)
@@ -27,9 +22,41 @@ def cache_zip(zip_path, dir_path):
 
 print(cache_zip('/Users/carlosinfante/Documents/Winc/files/data.zip',
                 '/Users/carlosinfante/Documents/Winc/files/cache'))
-'''
 
 
-# def cached_files():
+def cached_files():
+    arr = os.listdir('/Users/carlosinfante/Documents/Winc/files/cache')
+    return arr
 
-# def find_password()
+
+print(cached_files())
+
+
+def find_password(source_location):
+    search_string = 'password'
+    passd = ''
+    # list all the files in source location
+    direct = os.listdir(source_location)
+
+    # For loop to search for the string
+    for file in direct:
+        f = open(source_location + file, 'r')
+        if search_string in f.read():
+            print('file:', file)
+            file_path = open(source_location + file, 'r')
+
+            # Efficient way to search string in a large text file
+            for l_no, line in enumerate(file_path):
+
+                # search string
+                if search_string in line:
+                    print('string found in a file')
+                    print('Line Number:', l_no)
+                    print('Line:', line)
+                    index_pass = line.find(':') + 2
+                    passd = line[index_pass:]
+    return passd
+
+
+print('password is:', find_password(
+    '/Users/carlosinfante/Documents/Winc/files/cache/'))
